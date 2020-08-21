eb41d2f421a355081f8b0bfee17564232d318e9c
Vladimir <e3250@yandex.ru>
2020-08-21 12:32:26 +0500

    Обновление алгоритма

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {
     EVEN,
     MORE,
     GAP,
+    COLOR,
 } from './constants';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -21,11 +22,11 @@ import './App.css';
 import Footer from './components/Footer';
 
 const App = () => {
-    console.info(HEX_RECTANGLE_WIDTH);
     const [hexagon, setHexagon] = useState({
         settings: [],
         map: new Map(),
-        marked: [],
+        domains: new Map(),
+        elementDomain: new Map(),
     });
     const [probability, setProbability] = useState(0);
     const [sizes, setSizes] = useState({
@@ -37,7 +38,7 @@ const App = () => {
     useEffect(() => {
         const {L, M, N} = sizes;
         const hexMap = new Map();
-        const result = [];
+        const settings = [];
 
         let count = N;
 
@@ -52,21 +53,20 @@ const App = () => {
             y = (SIDE_LENGTH + HEX_HEIGHT) * (L - l);
             for (let i = 0; i < count; i++) {
                 const id = col + ' ' + row;
-                result.push({
+                settings.push({
                     x: x + HEX_RECTANGLE_WIDTH * i - HEX_RADIUS, // + GAP * col
                     y,
-                    color: '#389ce4',
+                    color: COLOR,
                     col,
                     row,
-                    value: id,
+                    value: '',
                     id,
                 });
-                hexMap.set(id, result[result.length - 1]);
+                hexMap.set(id, settings[settings.length - 1]);
                 col++;
             }
             col = 1;
             row++;
-
             count++;
         }
 
@@ -78,24 +78,23 @@ const App = () => {
             if (M - m < L) count--;
             for (let i = 0; i < count; i++) {
                 const id = col + ' ' + row;
-                result.push({
+                settings.push({
                     x: x + HEX_RECTANGLE_WIDTH * i, // + GAP * col),
                     y,
-                    color: '#389ce4',
+                    color: COLOR,
                     col,
                     row,
-                    value: id,
-                    // value: col++ + ' ' + row,
+                    value: '',
                     id,
                 });
-                hexMap.set(id, result[result.length - 1]);
+                hexMap.set(id, settings[settings.length - 1]);
                 col++;
             }
             col = 1;
             row++;
         }
 
-        setHexagon({...hexagon, settings: result, map: hexMap});
+        setHexagon({...hexagon, settings: settings, map: hexMap});
         // eslint-disable-next-line
     }, [sizes.L, sizes.M, sizes.N, sizes]);
 
@@ -107,73 +106,64 @@ const App = () => {
         return `rgb(${r}, ${g}, ${b})`;
     };
 
-    const checkHexagon = (curHex, markedHex) => {
-        if (curHex.row === markedHex.row && curHex.col === markedHex.col) {
-            return true;
-        }
+    const updateDomains = (el) => {
+        const foundDomainIds = [];
+        const {elementDomain, domains, map} = hexagon;
 
-        return false;
-    };
+        // Если 0
+        if (el.value === '') {
+            let neighborOffsets;
+            if (el.row < sizes.L) neighborOffsets = LESS;
+            if (el.row === sizes.L) neighborOffsets = EVEN;
+            if (el.row > sizes.L) neighborOffsets = MORE;
 
-    const markedHex = (marked, cur, map, type) => {
-        marked.forEach((element) => {
-            for (let i = 0; i < type.length; i++) {
-                const settings = {
-                    row: cur.row + type[i][1],
-                    col: cur.col + type[i][0],
+            // Цикл по соседям
+            neighborOffsets.forEach(([colOffset, rowOffset]) => {
+                const neighbor = {
+                    row: el.row + rowOffset,
+                    col: el.col + colOffset,
                 };
-                if (checkHexagon(settings, element.id)) {
-                    const color = element.id.color;
-                    marked.push({
-                        id: cur,
-                        domain: color,
-                    });
-                    cur.color = color;
-                    cur.value = '1';
-                    map.set(cur.id, cur);
-                    break;
+                const neighborId = neighbor.col + ' ' + neighbor.row;
+
+                // Если сосед входит в домен, сохраняем его
+                if (hexagon.elementDomain.has(neighborId)) {
+                    foundDomainIds.push(hexagon.elementDomain.get(neighborId));
                 }
-            }
-        });
+            });
 
-        return [marked, map];
-    };
+            // Объединяем найденные домены в один домен
+            // TODO Использовать другой id
+            const newDomainId = Date.now();
+            const newDomain = {elements: [el], color: randColor()};
+            elementDomain.set(el.id, newDomainId);
 
-    const paint = (notVisited) => {
-        let marked = hexagon.marked;
-        let map = hexagon.map;
-        for (let i = 0; i < notVisited.length; i++) {
-            const cur = notVisited[i];
-            if (marked.length === 0) {
-                const color = randColor();
-                marked.push({
-                    id: cur,
-                    domain: color,
+            new Set(foundDomainIds).forEach((domainId) => {
+                const domainElements = hexagon.domains.get(domainId).elements;
+                newDomain.elements.push(...domainElements);
+                // Удаляем старый домен
+                domains.delete(domainId);
+                // Обновляем id домена у элементов старого домена
+                domainElements.forEach((el) => {
+                    elementDomain.set(el.id, newDomainId);
                 });
-                cur.color = color;
-                cur.value = '1';
-                map.set(cur.id, cur);
-            } else {
-                if (cur.row < sizes.L) {
-                    markedHex(marked, cur, map, LESS);
-                } else if (cur.row === sizes.L) {
-                    markedHex(marked, cur, map, EVEN);
-                } else {
-                    markedHex(marked, cur, map, MORE);
-                }
-            }
+            });
+
+            // Красим элементы нового домена и обновляем значение
+            newDomain.elements.forEach((el) => {
+                const currentElement = map.get(el.id);
+                currentElement.color = newDomain.color;
+                currentElement.value = '1';
+            });
+
+            domains.set(newDomainId, newDomain);
+            setHexagon({...hexagon, map, domains, elementDomain});
         }
-        setHexagon({
-            ...hexagon,
-            map,
-            marked,
-        });
     };
 
     const onClick = (e) => {
         const currentHex = hexagon.map.get(e.target.id);
 
-        paint([currentHex]);
+        if (currentHex) updateDomains(currentHex);
     };
 
     return (
